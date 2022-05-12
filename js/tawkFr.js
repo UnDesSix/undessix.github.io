803b5505aab156723e9aa6e829392b7c2f38ae62
mlarboul <mlarboul@student.42.fr>
2022-05-12 10:58:38 +0200

    feat ADD CHAT IN FRENCH

---
+++ js/tawkFr.js
@@ -0,0 +1,9 @@
+ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+ (function(){
+ var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+ s1.async=true;
+ s1.src='https://embed.tawk.to/626e2e0fb0d10b6f3e702154/1g2rn2l8b';
+ s1.charset='UTF-8';
+ s1.setAttribute('crossorigin','*');
+ s0.parentNode.insertBefore(s1,s0);
+ })();
